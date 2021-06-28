5649198cd390c7484a71118842767ad5e695097c
scorebot <scorebot@outlook.com>
2021-06-28 10:58:33 +0800

    upgrade to 0.9.3

--- daas_client/version.py
+++ daas_client/version.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017-2019 AutoDeploy AI
+# Copyright (c) 2017-2021 AutoDeployAI
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-__version__ = '0.9.2'
+__version__ = '0.9.3'
